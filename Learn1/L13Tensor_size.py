6cd9e693774af9694264cd712e2aea10af208a7e
Luqiqy <lshihang1997@163.com>
2020-01-28 21:05:26 +0800

    Supplement

--- Learn1/L13Tensor_size.py
+++ Learn1/L13Tensor_size.py
@@ -1,4 +1,5 @@
-#利用Tensorflow中的函数shape得到张量的尺寸
+# 利用Tensorflow中的函数shape得到张量的尺寸
+'''
 import tensorflow.compat.v1 as tf
 t = tf.constant([[1, 2, 3],
                  [2, 0, 2],
@@ -6,3 +7,14 @@ t = tf.constant([[1, 2, 3],
 session = tf.Session()
 s = tf.shape(t)
 print("Tensor_size:", session.run(s))
+'''
+# 利用成员函数get_shape()或成员变量shape得到张量的尺寸
+import tensorflow.compat.v1 as tf
+t = tf.constant([[1, 2, 3],
+                 [2, 0, 2],
+                 [3, 2, 1]], tf.float32)
+#s = t.shape
+#print(s, type(s))
+
+a = t.get_shape()
+print(a, type(a))
