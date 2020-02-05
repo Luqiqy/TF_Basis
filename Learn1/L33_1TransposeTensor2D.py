009aca6a8a1d963fe4ea5627aa09c997d4582a4e
Luqiqy <lshihang1997@163.com>
2020-02-05 21:30:09 +0800

    Supplement

--- Learn1/L33_1TransposeTensor2D.py
+++ Learn1/L33_1TransposeTensor2D.py
@@ -5,6 +5,5 @@ x = tf.constant([[1, 2, 3],
 session = tf.Session()
 # 使用函数transpose进行张量转置
 r = tf.transpose(x, perm=[1, 0])
-
 print("转置前的二维张量：\n", session.run(x))
 print("转置后的二维张量：\n", session.run(r))
