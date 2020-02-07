653bede1942f6043b97a7a3c922b6f77add0ce9f
Luqiqy <lshihang1997@163.com>
2020-02-07 21:29:32 +0800

    Supplement

--- Learn1/L34_2ReshapeTensor2.py
+++ Learn1/L34_2ReshapeTensor2.py
@@ -3,7 +3,7 @@ import tensorflow.compat.v1 as tf
 t = tf.constant([[[0, 1], [2, 3], [4, 5]],
                  [[6, 7], [8, 9], [10, 11]]], tf.float32)
 session = tf.Session()
-# 使用reshape改变张量为2行6列
+# 使用reshape改变张量形状为2行6列
 t2 = tf.reshape(t, [2, -1])
 # 打印输出原张量及其形状
 print("张量原形状：\n", session.run(t))
