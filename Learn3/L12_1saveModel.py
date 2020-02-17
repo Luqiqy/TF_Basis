028cc137e6fe346de35bb362a41a0215b0a46c74
Luqiqy <lshihang1997@163.com>
2020-02-17 16:25:18 +0800

    Supplement

---
+++ Learn3/L12_1saveModel.py
@@ -0,0 +1,13 @@
+import tensorflow.compat.v1 as tf
+# 第一个variable，初始化为一个长度为3的一维张量
+v1 = tf.Variable(tf.constant([1, 2, 3], tf.float32), dtype=tf.float32, name='v1')
+# 第一个variable，初始化为一个长度为2的一维张量
+v2 = tf.Variable(tf.constant([4, 5], tf.float32), dtype=tf.float32, name='v2')
+# 声明一个tf.train.Saver对象
+saver = tf.train.Saver()
+# 创建会话，初始化变量
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+# 将变量v1和v2保存到./L2/model.ckpt路径下
+save_path = saver.save(sess, './L2/model.ckpt')
+sess.close()
